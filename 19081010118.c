8eb0f83b478cf8ad2b919c4519a63c4efcfa687c
an-Arkhan <arkhan.kak@gmail.com>
2020-09-23 22:32:29 +0700

    bismillah

---
+++ 19081010118.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("=====BIODATA=====\n");
+	printf("Nama			: Auliya Arkhan\n");
+	printf("NPM			: 19081010118\n");
+	printf("Tanggal lahir		: 22 Juli 2000\n");
+	printf("Alamat			: Jalan Kelapa Sari 16 Kota Madiun\n");
+	printf("Riwayat Pendidikan	: SMAN 2 Madiun\n");
+	return 0;
+}
